a5ce506167a9ad96e678ad55cbed12c9a57eac61
Bestest96 <jaluki@vp.pl>
2018-05-23 13:52:27 +0200

    Made Tree and Node classes generic, added value field to Node.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,14 +1,11 @@
 object Main {
   def main(args: Array[String]): Unit = {
-    var tree = Tree()
+    var tree = Tree[Int, String]()
     val ints = List(3, 6, 12, 28, 5, 36, 42, 16, 36, 10)
     for (i <- ints) {
-      val newTree = tree.add(i)
+      val newTree = tree.add(i, Option("XD"))
       tree = newTree
     }
-
-    tree.printBfs()
-
-//    tree.printInOrder()
+    tree.printInOrder()
   }
 }
--- src/main/scala/Tree.scala
+++ src/main/scala/Tree.scala
@@ -1,39 +1,40 @@
 import scala.annotation.tailrec
 
-case class Tree(root: Option[Node] = None, alpha: Double = 0.25) {
+case class Tree[K, V](root: Option[Node[K, V]] = None, alpha: Double = 0.25)(implicit ord: K => Ordered[K]) {
 
-  def add(elem: Int): Tree = {
+  def add(key: K, value: Option[V] = None): Tree[K, V] = {
     if (root.isEmpty)
-      Tree(Option(Node(elem)))
+      Tree(Option(Node[K, V](key, value)))
     else
-      this.copy(root = Option(addNode(elem, root.get)))
+      this.copy(root = Option(addNode(key, value, root.get)))
   }
 
-  private def addNode(elem: Int, node: Node): Node =
-    if (node.key >= elem) {
-      if (node.left.isEmpty) balance(node.copy(left = Option(Node(elem)), size = node.size + 1))
-      else balance(node.copy(left = Option(addNode(elem, node.left.get)), size = node.size + 1))
+  private def addNode(key: K, value: Option[V] = None, node: Node[K, V]): Node[K, V] =
+    if (node.key >= key) {
+      if (node.left.isEmpty) balance(node.copy(left = Option(Node(key, value)), size = node.size + 1))
+      else balance(node.copy(left = Option(addNode(key, value, node.left.get)), size = node.size + 1))
     }
     else {
-      if (node.right.isEmpty) balance(node.copy(right = Option(Node(elem)), size = node.size + 1))
-      else balance(node.copy(right = Option(addNode(elem, node.right.get)), size = node.size + 1))
+      if (node.right.isEmpty) balance(node.copy(right = Option(Node(key, value)), size = node.size + 1))
+      else balance(node.copy(right = Option(addNode(key, value, node.right.get)), size = node.size + 1))
+
     }
 
-//  def delete(elem: Int): Tree = {
-//    val toDelete = find(elem)
+//  def delete(key: Int): Tree = {
+//    val toDelete = find(key)
 //    if (toDelete.isEmpty)
 //      return this
-//    this.copy(root = Option(deleteNode(elem, this.root.get, None)))
+//    this.copy(root = Option(deleteNode(key, this.root.get, None)))
 //
 //
 //  }
 //
-//  private def deleteNode(elem: Int, node: Node, parent: Option[Node]): Node = {
-//    if (node.key > elem && node.left.isDefined)
-//      balance(node.copy(left = Option(deleteNode(elem, node.left.get, Option(node))), size = node.size))
-//    else if (node.key < elem && node.right.isDefined)
-//      balance(node.copy(right = Option(deleteNode(elem, node.right.get, Option(node))), size = node.size))
-//    else if (node.key == elem) {
+//  private def deleteNode(key: Int, node: Node, parent: Option[Node]): Node = {
+//    if (node.key > key && node.left.isDefined)
+//      balance(node.copy(left = Option(deleteNode(key, node.left.get, Option(node))), size = node.size))
+//    else if (node.key < key && node.right.isDefined)
+//      balance(node.copy(right = Option(deleteNode(key, node.right.get, Option(node))), size = node.size))
+//    else if (node.key == key) {
 //      (node.left, node.right) match {
 //        case (None, None) => {
 //          val leftParentChild = if (parent.get.left.isDefined && parent.get.left.get == node) None else parent.get.left
@@ -44,22 +45,26 @@ case class Tree(root: Option[Node] = None, alpha: Double = 0.25) {
 //    }
 //  }
 
-  def find(elem: Int): Option[Node] = {
+  def find(key: K): Option[Node[K, V]] = {
 
     @tailrec
-    def searchInTree(elem: Int, node: Option[Node]): Option[Node] = {
+    def searchInTree(key: K, node: Option[Node[K, V]]): Option[Node[K, V]] = {
       if (node.isEmpty)
         None
-      else node.get.key - elem match {
-        case 0 => node
-        case x if x > 0 => searchInTree(elem, node.get.left)
-        case x if x < 0 => searchInTree(elem, node.get.right)
+      else if (node.get.key > key) {
+        searchInTree(key, node.get.left)
+      } else {
+        if (node.get.key == key) {
+          node
+        } else {
+          searchInTree(key, node.get.right)
+        }
       }
     }
-    searchInTree(elem, root)
+    searchInTree(key, root)
   }
 
-  def rotateRight(startNode: Option[Node]): Node = {
+  def rotateRight(startNode: Option[Node[K, V]]): Node[K, V] = {
     val startNodeL = startNode.get.left
     val startNodeLR = startNodeL.get.right
     val startNodeLRSize = startNodeLR match {
@@ -77,7 +82,7 @@ case class Tree(root: Option[Node] = None, alpha: Double = 0.25) {
     startNodeL.get.copy(right = Option(startNode.get.copy(left = startNodeLR, size = startNodeSize)), size = startNodeLSize)
   }
 
-  def rotateLeft(startNode: Option[Node]): Node = {
+  def rotateLeft(startNode: Option[Node[K, V]]): Node[K, V] = {
     val startNodeR = startNode.get.right
     val startNodeRL = startNodeR.get.left
     val startNodeRLSize = startNodeRL match {
@@ -95,7 +100,7 @@ case class Tree(root: Option[Node] = None, alpha: Double = 0.25) {
     startNodeR.get.copy(left = Option(startNode.get.copy(right = startNodeRL, size = startNodeSize)), size = startNodeRSize)
   }
 
-  def balance(node: Node): Node = {
+  def balance(node: Node[K, V]): Node[K, V] = {
     val leftWeight = node.left match {
       case None => 1
       case _ => node.left.get.weight()
@@ -113,31 +118,21 @@ case class Tree(root: Option[Node] = None, alpha: Double = 0.25) {
 
   def printInOrder(): Unit = {
     if (root.get.left.isDefined) Tree(root.get.left).printInOrder()
-    println(root.get.key)
+    println("key: " + root.get.key + ", value: " + root.get.value.getOrElse("None"))
     if (root.get.right.isDefined) Tree(root.get.right).printInOrder()
   }
 
   def printBfs(): Unit = {
-    def recursive(s1: Stream[Node], s2: Stream[Node],
-                  output: Seq[Stream[Node]], f: Node => Stream[Node]): Seq[Stream[Node]] = {
-      (s1, s2) match {
-        case (x1: Stream[Node], x2: Stream[Node]) if x1.isEmpty && x2.isEmpty => output
-        case (x1: Stream[Node], x2: Stream[Node]) if x1.isEmpty => recursive(x2, Stream(), output :+ x2, f)
-        case _ => recursive(s1.tail, s2 append f(s1.head), output, f)
-      }
+    def recursive(s: Stream[Node[K, V]], f: Node[K, V] => Stream[Node[K, V]]): Stream[Node[K, V]] = {
+      if (s.isEmpty) s
+      else s.head #:: recursive(s.tail append f(s.head), f)
     }
-    
-    def filterNoneChildren(node: Node): Stream[Node] = {
+    def filterNoneChildren(node: Node[K, V]): Stream[Node[K, V]] = {
       val filteredChildren = Stream(node.left, node.right).filter(_.isDefined).map(_.get)
       filteredChildren
     }
 
-    if (root.isDefined) {
-      val output = recursive(Stream(), Stream(root.get), Seq(), filterNoneChildren)
-      for(s: Stream[Node] <- output){
-        s.map(_.key).foreach( (x: Int) => { print(x); print(" ") })
-        println()
-      }
-    }
+    if (root.isDefined)
+      recursive(Stream(root.get), filterNoneChildren).map(_.key).foreach(println)
   }
 }
